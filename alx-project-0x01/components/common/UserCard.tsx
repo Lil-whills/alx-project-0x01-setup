2e2a64e590f0f8e99cdd41b9977058cd0364b724
Williams Ameyaw <williams.kyere@stu.ucc.edu.gh>
2025-07-17 13:55:53 -0400

    completed task 3

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,22 @@
+import { UserProps } from "@/interfaces";
+
+const UserCard: React.FC<UserProps> = ({id, name, username, email, phone, website, address, company }) => {
+  return (
+    <div className="max-w-md mx-auto mx-auto my-4 p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+      <div className="mb-4">
+        <h2 className="text-2xl font-semibold text-gray-800">{name} (@{username})</h2>
+        <p className="text-gray-600">{email}</p>
+        <p className="text-gray-600">phone: {phone}</p>
+        <p className="text-gray-600">Website: {website}</p>
+      </div>
+
+      <div className="mt-4 text-sm text-gray-500">
+        <p><strong>Address:</strong>{address.street}, {address.suite}, {address.city}, {address.zipcode}</p>
+        <p><strong>Geo:</strong> Lat: {address.geo.lat}, Lng: {address.geo.lng}</p>
+        <p><strong>Company:</strong> {company.name}</p>
+        <p><strong>Catch Phrase:</strong> "{company.catchPhrase}"</p>
+      </div>
+    </div>
+  )
+}
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,40 @@
-import Header  from "@/components/layout/Header" 
+import React from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+type Props = {
+  posts: User[];
+};
+
+const UsersPage = ({ posts }: Props) => {
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {posts.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> (@{user.username}) - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default UsersPage;
